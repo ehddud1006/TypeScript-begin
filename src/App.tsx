930366bbdeaa60f77af181400128e4f52fcdbae1
ehddud1006 <62373865+ehddud1006@users.noreply.github.com>
2022-04-24 23:08:21 +0900

    ddd

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-
+import InputFeild from './components/inputFeild';
+import { Todo } from './model';
 let name:string = "Piyush"
 let age:number = 5
 let isStudent: boolean
 const App:React.FC = () => {
+  const [todo, setTodo] = useState<string>("")
+  const [todos,setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+  }
   return (
     <div className="App">
       <span className="heading">Taskify</span>
+      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}></InputFeild>
     </div>
   );
 }
---
+++ src/components/inputFeild.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './styles.css'
+
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd : () => void
+}
+
+const inputFeild:React.FC<Props> = ({todo, setTodo,handleAdd}) => {
+  return (
+    <form className='input' onSubmit={handleAdd}>
+        <input className='input__box' type="input" 
+        placeholder='Enter a task'
+        value={todo}
+        onChange={(e)=>setTodo(e.target.value)}></input>
+        <button className='input_submit' type="submit">Go</button>
+    </form>
+  )
+}
+
+export default inputFeild
